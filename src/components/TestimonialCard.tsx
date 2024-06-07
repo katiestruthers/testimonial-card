a2952f3a1f36792bf6118e6b7d12f5118bdfdefb
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-06 22:44:34 -0500

    fix styling, images

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,15 +1,17 @@
+import profileImage from "../img/profile-thumbnail.png";
+
 export const TestimonialCard = () => {
   return (
     <article className="card">
       <header>
-        <img src="img/profile-thumbnail.png" alt="User profile thumbnail" />
+        <img src={profileImage} alt="User profile thumbnail" />
         <span>
           <div className="text-lg text-neutral-900 font-semibold">Sarah Dole</div>
           <div className="text-sm text-neutral-600">@sarahdole</div>
         </span>
-        <p className="text-base text-neutral-600">I've been searching for high-quality abstract images for my design projects, 
-        and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!</p>
       </header>
+      <p className="text-base text-neutral-600">I've been searching for high-quality abstract images for my design projects, 
+      and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!</p>
     </article>
   )
 };
