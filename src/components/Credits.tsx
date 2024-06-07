d5ce25c4c102bdb8bafc953c070daaeff35d0caf
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-06 22:24:04 -0500

    convert HTML & styling into React components

---
+++ src/components/Credits.tsx
@@ -0,0 +1,17 @@
+export const Credits = () => {
+  return (
+    <div className="credits" data-gfe-screenshot-exclude="true">
+      A challenge by
+      <a
+        href="https://www.greatfrontend.com/projects?ref=challenges"
+        target="_blank"
+        rel="noreferrer"
+        > GreatFrontEnd Projects</a>. Built by
+      <a
+        href="https://www.greatfrontend.com/projects/u/username"
+        target="_blank"
+        rel="noreferrer"
+        > Katie Struthers</a>.
+    </div>
+  );
+};
